cc4e3fd48c241b098ceb72de1031ecf960933cc0
flores <daniel.rojas.cb3@gmail.com>
2024-06-10 12:33:17 -0600

    news

--- p55.cpp
+++ p55.cpp
@@ -1,17 +1,66 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
-int main()
-{
-    int an;
-    cout<<"BIENVENIDO A ESTE PROGRAMA\n";
-    cout<<"¿Desea agregar una cuenta o sigue como usario anónimo\n?";
-    cout<<"1)agregar cuenta\n"<<"seguir como anónimo\n"; cin>>an;
-    if (an=1)
-    {
-        cout<<"";
+int main(){
+  string admin[20];
+  string user[20];
+  string anonimo[20];
+
+  stirng name[20];//nombe anonimo
+  string root[] = "admin";
+  string pass_ad[] = "1234";//esta es la contraseañ del admin
+  string user,user_password;
+  string user_identificador[]=user;
+  string pass_usuario[]=user_password;
+  char s;
+
+
+  cout<<"ingresa tu tipo de usuario";
+  switch(int type){
+    cin >> type;
+    case 1://usuario
+      cout<<"ingresa tu nombre de usuario";
+      cin >> user;
+      cout<<"Ingresa tu contraseña: ";
+      cin>> user_password;
+      break;
+    case 2://admin
+      cout << "Ingresa tu nombre: " << endl;
+      cin >> admin;
+
+      cout << "Ingresa la contraseña" << endl;
+      cin >> password;
+      if ((strmpc(user,admin)==0)&&(strcmp(pass,1234)){
+      cout<<"Bienvenido Administrador "<<endl
+      cout<<"Tienes las siguientes ventajas: "<<endl;
+      cout<<"1.Modificar mis datos personales\n";
+      cout<<"2.Mostrar mis vendedores recomendados\n";
+       cout<<"3.Recomendar a un vendedor\n";
+        cout<<"4.Mostrar todos los vendedores recomendados por un usuario\n";
+        cout<<"5.Ver los productos ofrcidos por un vendedor\n";
+        cout<<"6. Mostrar todos los productos a la venta\n";
+        
+      
+    
+  }
+
+  if(strmpc(user,anonimo)==0){
+   cout<<"Ingresaste como anonimo";
+    cout<<"ingresa tu nombre en el que quieres acceder: ";
+    cin>>name;
+    cout<<"bienvenido al sistema "<<name;
+    cout<<"\n productos a la venta";
+    cout<<"\n pulsa s para salir";
+    cin>>s;
+    if(s==´s´){
+      break;
     }
-    else if (an=2){
-        cout<<"usted es un usuario registrado";
-    }
-    return 0;
+    
+    
+  }
+  if(strcmp(user,user) == 0){
+    cout<<"Bienvenido Usuario "<<endl
+    cout<<"Bienvenido Administrador "<<endl
+    } 
+      }
 }
