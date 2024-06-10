b24c357e3f25792d1d2a7124b5a628d230e74b66
flores <daniel.rojas.cb3@gmail.com>
2024-06-10 12:25:00 -0600

    news

---
+++ p55.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int an;
+    cout<<"BIENVENIDO A ESTE PROGRAMA\n";
+    cout<<"¿Desea agregar una cuenta o sigue como usario anónimo\n?";
+    cout<<"1)agregar cuenta\n"<<"seguir como anónimo\n"; cin>>an;
+    if (an=1)
+    {
+        cout<<"";
+    }
+    else if (an=2){
+        cout<<"usted es un usuario registrado";
+    }
+    return 0;
+}
